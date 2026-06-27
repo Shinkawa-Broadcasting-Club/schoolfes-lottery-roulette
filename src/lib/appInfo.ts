d31c54eee612a554607917f57ca236d061ab0b1b
Lasxle <94899581+nw3q@users.noreply.github.com>
2026-06-27 23:54:51 +0900

    replace: school-fes to schoolfes

--- src/lib/appInfo.ts
+++ src/lib/appInfo.ts
@@ -5,5 +5,5 @@ export const APP_VERSION = packageJson.version;
 export const APP_AUTHOR = 'Koutarou Nishiwaki';
 export const APP_COPYRIGHT = '©2024-2026 Koutarou Nishiwaki';
 export const APP_GITHUB_URL =
-  'https://github.com/Shinkawa-Broadcasting-Club/school-fes-lottery-roulette';
+  'https://github.com/Shinkawa-Broadcasting-Club/schoolfes-lottery-roulette';
 export const APP_LICENSE = 'MIT';
